ddd1d8eb396fd4b320c5beaa1e8be4dc7f9a126d
Vinicius <vinicius.souza@manaus.am.gov.br>
2024-08-01 15:35:08 -0400

    refactor: movendo o método registraLink.
    Movendo o método registraLink da classe controller para service e criando o método registraLinkPayload.

--- src/main/java/com/rocketseat/planner/link/LinkController.java
+++ src/main/java/com/rocketseat/planner/link/LinkController.java
@@ -1,7 +1,6 @@
 package com.rocketseat.planner.link;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,38 +10,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.rocketseat.planner.trip.Trip;
-import com.rocketseat.planner.trip.TripService;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 @RequestMapping("/link")
 public class LinkController {
-	
+
 	@Autowired
 	private LinkService linkService;
-	
-	@Autowired
-	private TripService tripService;
-	
+
 	@PostMapping("/{id}")
 	public ResponseEntity<LinkResponse> registerLink(@PathVariable UUID id, @RequestBody LinkRequestPayload payload) {
-		Optional<Trip> trip = this.tripService.getTrips(id);
-
-		if (trip.isPresent()) {
-			Trip rawTrip = trip.get();
-
-			LinkResponse linkResponse = this.linkService.registerLink(payload, rawTrip);
-
-			return ResponseEntity.ok(linkResponse);
-		}
-
-		
-		return ResponseEntity.notFound().build();
+		return linkService.registraLink(id, payload);
 	}
-	
+
 	@GetMapping("/{id}")
 	public ResponseEntity<List<LinkData>> getAllLinks(@PathVariable UUID id) {
 		List<LinkData> linkDataList = this.linkService.getAllLinksFromId(id);
--- src/main/java/com/rocketseat/planner/link/LinkService.java
+++ src/main/java/com/rocketseat/planner/link/LinkService.java
@@ -4,8 +4,10 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.rocketseat.planner.trip.Trip;
+import com.rocketseat.planner.trip.TripService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -14,8 +16,11 @@ public class LinkService {
 
 	@Autowired
 	private LinkRepository linkRepository;
+	
+	@Autowired
+	private TripService tripService;
 
-	public LinkResponse registerLink(LinkRequestPayload payload, Trip trip) {
+	public LinkResponse registraLinkPayload(LinkRequestPayload payload, Trip trip) {
 		Link newLink = new Link(payload.title(), payload.url(), trip);
 
 		this.linkRepository.save(newLink);
@@ -23,6 +28,21 @@ public class LinkService {
 		log.info("criando um link...");
 		return new LinkResponse(newLink.getId());
 
+	}
+	
+	public ResponseEntity<LinkResponse> registraLink(UUID id, LinkRequestPayload payload) {
+		Optional<Trip> trip = this.tripService.getTrips(id);
+
+		if (trip.isPresent()) {
+			Trip rawTrip = trip.get();
+
+			LinkResponse linkResponse = registraLinkPayload(payload, rawTrip);
+
+			return ResponseEntity.ok(linkResponse);
+		}
+
+		
+		return ResponseEntity.notFound().build();
 	}
 
 	public List<LinkData> getAllLinksFromId(UUID tripId) {
